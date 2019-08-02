62561f539ca0a12a2d3d400e7f2adbfbd9e85190
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-08-02 08:15:37 -0700

    Updated

---
+++ Projects/WonderWoman/test.java
@@ -0,0 +1,15 @@
+public class test {
+    public static void main(String[] args){
+
+    System.out.println("Hello World!");
+    int myVariable = 1023089000;
+    System.out.println(myVariable);
+    String Asentence = "Hello Saanjh, how is your day going?";
+    System.out.println(Asentence);
+    }
+
+
+
+
+
+}
