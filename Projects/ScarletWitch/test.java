6c2743da235e9566d41378186099c68df0dbba09
EUReKACamper7 <51757403+EUReKACamper@users.noreply.github.com>
2019-08-02 16:39:02 -0700

    week 5

---
+++ Projects/ScarletWitch/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int EthansVariable = 21;
+        System.out.println(EthansVariable);
+        String meSentence = "WoWeWoWe";
+        
+    }
+}
