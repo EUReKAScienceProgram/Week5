1bff247524e47930b87ddfa8aabde348b70e38dd
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-08-02 16:31:08 -0700

    Week 5

---
+++ Projects/Hawkeye/test.java
@@ -0,0 +1,26 @@
+public class test {
+    public static void main(String[]args){
+        System.out.println("Hello World");
+
+
+        myVariable = 123;
+
+
+        System.out.println
+        System.out.println(myVariable);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
