ed3bd63b91373d654d12901d5568553364fbddc1
EUReKACamper14 <51757403+EUReKACamper@users.noreply.github.com>
2019-08-02 16:24:17 -0700

    week 5

---
+++ Projects/Wasp/Test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args){
+System.out.println("HELLO WORLD IM BACK" );
+int myVariable = 17;
+        System.out.println(myVariable);
+String mySentence = "FBI protect these people at all costs";
+
+    }
+
+}
